178ff2cbde40684296cc026d35fc2d6066a69729
GuteeQ <wiktor.gut08@icloud.com>
2023-05-06 16:25:36 +0200

    New file - MailcapParseException.java (4/1600)

---
+++ com/GuTeK/activations/registeries/MailcapParseException.java
@@ -0,0 +1,22 @@
+/*
+    Author: .GuTeK <dev@gutcode.pl>
+    Project: AUTH [MINECRAFT PLUGIN PASS]
+    Price: MINECRAFT PLUGIN PASS - $$$
+    Resources: 4/1600
+    Data: 06.05.2023
+    Contact Discord: .GuTeK#0001
+    Contact e-mail: dev@gutcode.pl
+    Our websites: https://gutcode.pl
+    ⓒ 2023 by .GuTeK | ALL RIGHTS RESERVED |
+*/
+
+package GuTeK.activations.registeries;
+
+public class MailcapParseException extends Exception {
+    public MailcapParseException(){
+    }
+
+    public MailcapParseException(final String inInfo) {
+        super(inInfo);
+    }
+}
